8edc16e262c4ec91bc4dec182a2398514c0eb0be
Kian1212 <k.karimipor@gmail.com>
2019-09-09 19:58:48 -0700

    Split data to Train , Test and Valid

--- Breast Cancer.py
+++ Breast Cancer.py
@@ -21,9 +21,15 @@ data = raw_data.iloc[:, :9].values
 lables = raw_data.iloc[:, 9]
 
 """
-Split to Train , Test and Valid
+Split data to Train , Test and Valid
 """
 
+X_train, X_test,Y_train, Y_test = train_test_split(data, lables, test_size=0.3, shuffle=True)
+X_train, X_valid,Y_train, Y_valid = train_test_split(X_train, Y_train, test_size=0.2, shuffle=True)
 
 
+
+"""
+
+"""
 print('End!!')
