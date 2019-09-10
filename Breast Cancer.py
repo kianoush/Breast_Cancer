8712a552fc83e59681ece987e935f54727a1e31d
Kian1212 <k.karimipor@gmail.com>
2019-09-09 19:40:15 -0700

    Initial commit

---
+++ Breast Cancer.py
@@ -0,0 +1,20 @@
+"""
+Breast Cancer
+"""
+
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import torch
+import torch.nn as nn
+import os
+
+"""
+import data
+"""
+path = os.listdir()
+
+print (path)
+
+
+print('End!!')
