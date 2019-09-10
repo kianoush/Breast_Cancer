013127ead00ecf8dd4080bd290ff67f33437b16e
Kian1212 <k.karimipor@gmail.com>
2019-09-09 21:55:43 -0700

    Train

--- Breast Cancer.py
+++ Breast Cancer.py
@@ -26,13 +26,13 @@ Split data to Train , Test and Valid
 """
 
 X_train, X_test, Y_train, Y_test = train_test_split(data, lables, test_size=0.2, shuffle=True)
-X_train, X_valid, Y_train1, Y_valid = train_test_split(X_train, Y_train, test_size=0.2, shuffle=True)
+X_train, X_valid, Y_train, Y_valid = train_test_split(X_train, Y_train, test_size=0.2, shuffle=True)
 
 X_train = torch.tensor(X_train).float()
 X_test = torch.tensor(X_test).float()
 X_valid = torch.tensor(X_valid).float()
 
-Y_train = torch.tensor(Y_train1).long()
+Y_train = torch.tensor(Y_train).long()
 Y_test = torch.tensor(Y_test).long()
 Y_valid = torch.tensor(Y_valid).long()
 
@@ -42,36 +42,53 @@ Model
 
 num_class = 2
 num_featurs = X_train.shape[1]
-num_hiddenl = 10
+num_hiddenl = 5
 
 model = torch.nn.Sequential(torch.nn.Linear(num_featurs, num_hiddenl),
                             torch.nn.ReLU(),
-                            torch.nn.Linear(num_hiddenl, num_class)
+                            torch.nn.Linear(num_hiddenl, num_class),
+                            torch.nn.Sigmoid()
 )
 
 
 """
 Loss
 """
-loss = torch.nn.BCELoss()
+loss = torch.nn.CrossEntropyLoss()
 
 """
 Optimizer
 """
-optim = torch.optim.Adam(model.parameters(), lr=0.001)
+optimizer = torch.optim.Adam(model.parameters(), lr=0.001)
 
 
 num_sample_train = torch.tensor(X_train.shape[0])
 num_sample_test = torch.tensor(X_test.shape[0])
 num_sample_valid = torch.tensor(X_valid.shape[0])
 
-num_epochs = 100
+num_epochs = 200
 for epoch in range(num_epochs):
-    optim = optim.zero_grad()
+    optim = optimizer.zero_grad()
     Y_pred = model(X_train)
     loss_value = loss(Y_pred, Y_train)
 
+    num_corrects = torch.sum(torch.max(Y_pred, 1)[1] == Y_train)
+    acc_train = num_corrects.float() / float(num_sample_train)
+
     loss_value.backward()
-    optim.step()
+    optimizer.step()
+
+    yp = model(X_valid)
+    num_corrects = torch.sum(torch.max(yp, 1)[1]==Y_valid)
+    acc_valid = num_corrects.float() / float(num_sample_valid)
+    print("Epoch: ", epoch, 'Train Loss: ', loss_value.item(), 'Train Accurecy: ', acc_train.item(), 'VALIDATION acoreccy', acc_valid.item())
+
+
+
+yp = model(X_test)
+num_corrects = torch.sum(torch.max(yp, 1)[1]==Y_test)
+acc_test = num_corrects.float() / float(num_sample_valid)
+print('Test Accurecy: ', acc_test.item())
+
 
 print('End!!')
